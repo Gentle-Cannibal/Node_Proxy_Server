490a26ec17c98838fcc1fcb35899cb84581fe4ae
Dmytro Chornomaz <d.chornomaz@godeltech.com>
2024-12-02 13:18:03 +0100

    Tasks 1-8

---
+++ utils/errorHandler.js
@@ -0,0 +1,27 @@
+import {
+  consoleFontColors,
+  statusCodes,
+  userMessages,
+} from "../config/constants.js";
+import SourceError from "../errors/source.error.js";
+
+const { RED } = consoleFontColors;
+const { INTERNAL_SERVER_ERROR } = statusCodes;
+const { ERROR_HAPPENED_MESSAGE } = userMessages;
+
+function errorHandler(error, request, response, next) {
+  if (error instanceof SourceError) {
+    console.error(RED, error.stack);
+    response
+      .status(error.argument)
+      .json({ error: ERROR_HAPPENED_MESSAGE, statusCode: error.argument });
+  } else {
+    console.error(RED, error.stack);
+    response.status(INTERNAL_SERVER_ERROR).json({
+      error: ERROR_HAPPENED_MESSAGE,
+      statusCode: INTERNAL_SERVER_ERROR,
+    });
+  }
+}
+
+export default errorHandler;
